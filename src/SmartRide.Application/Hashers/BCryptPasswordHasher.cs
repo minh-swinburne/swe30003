41573f07dc7680992aaad54ae78df4584a2e5304
minh-swinburne <140883075+minh-swinburne@users.noreply.github.com>
2025-04-02 10:37:25 +0700

    feat(app): add password hasher

---
+++ src/SmartRide.Application/Hashers/BCryptPasswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SmartRide.Domain.Entities;
+
+namespace SmartRide.Application.Hashers;
+
+public class BCryptPasswordHasher<TUser> : IPasswordHasher<TUser> where TUser : BaseEntity
+{
+    public string HashPassword(TUser user, string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentNullException(nameof(password));
+        }
+
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+    {
+        if (string.IsNullOrEmpty(hashedPassword))
+        {
+            throw new ArgumentNullException(nameof(hashedPassword));
+        }
+        if (string.IsNullOrEmpty(providedPassword))
+        {
+            throw new ArgumentNullException(nameof(providedPassword));
+        }
+
+        return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword)
+            ? PasswordVerificationResult.Success
+            : PasswordVerificationResult.Failed;
+    }
+}
