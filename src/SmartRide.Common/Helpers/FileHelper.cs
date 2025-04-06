177098efb12f24990a087f8915f940b1e2a57872
minh-swinburne <140883075+minh-swinburne@users.noreply.github.com>
2025-04-06 10:09:49 +0700

    feat(common): use csvhelper

--- src/SmartRide.Common/Helpers/FileHelper.cs
+++ src/SmartRide.Common/Helpers/FileHelper.cs
@@ -1,4 +1,6 @@
-using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace SmartRide.Common.Helpers;
 
@@ -6,55 +8,15 @@ public static class FileHelper
 {
     public static void WriteToCsv<T>(IEnumerable<T> records, string filePath)
     {
-        using var writer = new StreamWriter(filePath, false, Encoding.UTF8);
-        var properties = typeof(T).GetProperties();
-
-        // Write header
-        writer.WriteLine(string.Join(",", properties.Select(p => p.Name)));
-
-        // Write records
-        foreach (var record in records)
-        {
-            var values = properties.Select(p => p.GetValue(record)?.ToString()?.Replace(",", " ") ?? string.Empty);
-            writer.WriteLine(string.Join(",", values));
-        }
+        using var writer = new StreamWriter(filePath);
+        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+        csv.WriteRecords(records);
     }
 
-    public static List<T> ReadFromCsv<T>(string filePath) where T : new()
+    public static List<T> ReadFromCsv<T>(string filePath) where T : class
     {
-        var records = new List<T>();
-        var properties = typeof(T).GetProperties();
-
-        using var reader = new StreamReader(filePath, Encoding.UTF8);
-        var header = reader.ReadLine()?.Split(',');
-
-        if (header == null)
-            throw new InvalidOperationException("CSV file is empty or invalid.");
-
-        var propertyMap = header
-            .Select((name, index) => new { Name = name, Index = index })
-            .Where(h => properties.Any(p => p.Name == h.Name))
-            .ToDictionary(h => h.Name, h => h.Index);
-
-        while (!reader.EndOfStream)
-        {
-            var values = reader.ReadLine()?.Split(',');
-            if (values == null) continue;
-
-            var record = new T();
-            foreach (var property in properties)
-            {
-                if (propertyMap.TryGetValue(property.Name, out var index) && index < values.Length)
-                {
-                    var value = values[index];
-                    var convertedValue = Convert.ChangeType(value, property.PropertyType);
-                    property.SetValue(record, convertedValue);
-                }
-            }
-
-            records.Add(record);
-        }
-
-        return records;
+        using var reader = new StreamReader(filePath);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+        return csv.GetRecords<T>().ToList();
     }
 }
