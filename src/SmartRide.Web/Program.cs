b530991f02f0acd78b3272979f32c2553d0e0dd3
dvmh123 <dvmh2k3@gmail.com>
2025-04-09 20:38:00 +0700

    Fix(FrontEnd): Fix typo

--- src/SmartRide.Web/Program.cs
+++ src/SmartRide.Web/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Register HttpClient
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddre   ss) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Register services
 builder.Services.AddScoped<IUserService, UserService>();
